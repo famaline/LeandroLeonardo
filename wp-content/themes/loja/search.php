a2c0ba875a292c6ecab64ceb9f30fd76dd9d8cc3
Leandro Miserani <leandro.miserani@abril.com.br>
2011-07-17 15:17:38 -0300

    adicionado link nos termos de busca

--- wp-content/themes/loja/search.php
+++ wp-content/themes/loja/search.php
@@ -12,51 +12,53 @@ get_header(); ?>
 		<div id="container">
 			<div id="content" role="main">
         <div>
-        <table border="0" cellspacing="0" cellpadding="0" width="100" height="100&quot;">
+        <table border="0" cellspacing="0" cellpadding="0" width="120" height="100&quot;">
         <tbody>
         <tr>
-        <td style="text-align: left;" align="left">
+        <td style="text-align: left;" align="left">&nbsp;
+
           <strong>Tipo</strong>
-          </br>
-          Botas 
-          Cano Longo 
-          Festa 
-          Mocassim 
-          Peep Toe 
-          Rasteiras 
-          Salto Alto 
-          Sandálias 
-          Sapatilhas 
-          Scarpin 
-          Tênis
+          <a href="?s=Botas">Botas</a>
+          <a href="?s=Festa">Festa</a>
+          <a href="?s=Mocassim">Mocassim</a>
+          <a href="?s=Peep Toe">Peep Toe</a>
+          <a href="?s=Rasteira">Rasteiras</a>
+          <a href="?s=Salto Alto">Salto Alto</a>
+          <a href="?s=Sandália">Sandálias</a>
+          <a href="?s=Sapatilha">Sapatilhas</a>
+          <a href="?s=Scarpin">Scarpin</a> 
+          <a href="?s=Tênis">Tênis</a>  
           <strong>Tamanho</strong>
-          25 
-          26 
-          27 
-          28 
-          31 
-          33 
-          34 
-          35 
-          36 
-          37 
-          38
-          39 
-          40
+          <a href="?s=25">25</a>
+          <a href="?s=26">26</a>
+          <a href="?s=27">27</a>
+          <a href="?s=28">28</a>
+          <a href="?s=31">31</a>
+          <a href="?s=33">33</a>
+          <a href="?s=34">34</a>
+          <a href="?s=35">35</a>
+          <a href="?s=36">36</a>
+          <a href="?s=37">37</a>
+          <a href="?s=38">38</a>
+          <a href="?s=39">39</a>
+          <a href="?s=40">40</a>
        </td>
         <td align="left">
         <table border="0" cellspacing="8" cellpadding="8" width="900" height="302">
         <tbody>
         <tr>
         <td>   <?php if ( have_posts() ) : ?>
-          				<h1 class="page-title"><?php printf( __( 'Search Results for: %s', 'twentyten' ), '<span>' . get_search_query() . '</span>' ); ?></h1>
+          				<h2 class="entry-title"><?php printf( __( 'Search Results for: %s', 'twentyten' ), '<span>' . get_search_query() . '</span>' ); ?></h1>
+          				<div class="entry-content">
           				<?php
           				/* Run the loop for the search to output the results.
           				 * If you want to overload this in a child theme then include a file
           				 * called loop-search.php and that will be used instead.
           				 */
+          				 
           				 get_template_part( 'loop', 'search' );
           				?>
+          				</div>
           <?php else : ?>
           				<div id="post-0" class="post no-results not-found">
           					<h2 class="entry-title"><?php _e( 'Nothing Found', 'twentyten' ); ?></h2>
