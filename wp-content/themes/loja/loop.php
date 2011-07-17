de230d78ee6226d4e87c925b8291fb3e0bedd28d
Leandro Miserani <leandro.miserani@abril.com.br>
2011-07-17 17:14:53 -0300

    Removendo comentário

--- wp-content/themes/loja/loop.php
+++ wp-content/themes/loja/loop.php
@@ -161,12 +161,10 @@
 					</span>
 					<span class="meta-sep">|</span>
 				<?php endif; ?>
-				<span class="comments-link"><?php comments_popup_link( __( 'Leave a comment', 'twentyten' ), __( '1 Comment', 'twentyten' ), __( '% Comments', 'twentyten' ) ); ?></span>
-				<?php edit_post_link( __( 'Edit', 'twentyten' ), '<span class="meta-sep">|</span> <span class="edit-link">', '</span>' ); ?>
 			</div><!-- .entry-utility -->
 		</div><!-- #post-## -->
 
-		<?php comments_template( '', true ); ?>
+	
 
 	<?php endif; // This was the if statement that broke the loop into three parts based on categories. ?>
 
---
+++ wp-content/themes/loja/searchform.php
@@ -0,0 +1,6 @@
+<form role="search" method="get" id="searchform">
+<div>
+<input type="text" value="" name="s" id="s" />
+<input type="submit" id="searchsubmit" value="BUSCAR" />
+</div>
+</form>
