eafc59b8546ccc6ad301c0a790f109232fa8dc25
Leandro Miserani <leandro.miserani@abril.com.br>
2011-07-20 21:55:54 -0300

    Mais todo

--- wp-content/themes/loja/todo.php
+++ wp-content/themes/loja/todo.php
@@ -4,4 +4,10 @@
   -------------------------
   <li>Fiz a pesquisa pensando que todos os atributos da pesquisa é uma categoria. Assim, vai ter que ter as categorias       dos tipos do produto</li>
   <li>Ex: O produto Sandália 2011 vai estar associado a Lancamento, Sandália, Primavera-Verão, etc. Se o cliente digitar qualquer um desses itens, vai aparecer esse produlo </li>
+  ------------------
+  <li>Mandei um e-mail da páginal institucional com todas as instruçoes</li>
+  <li>Mandei um e-mail  da página amigos de di'rô com todas as instruçoes</li>
+  <li>Outro e-mail com Localização	</li>
+   <li>Mais um.  Contato	</li>
+  
 </ol>
