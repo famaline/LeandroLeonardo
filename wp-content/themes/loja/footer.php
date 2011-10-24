cafee5d4a9befac46c61b351ac2d5b2d3b0bfb51
Leonardo Campos <campos.leonardo@hotmail.com>
2011-10-24 20:49:39 -0200

    Links para redes sociais

--- wp-content/themes/loja/footer.php
+++ wp-content/themes/loja/footer.php
@@ -17,6 +17,12 @@
     <td id="aceitamos">Aceitamos:</td>
     <td id="pgto">&nbsp;</td>
     <td id="texto"><?php Renders::render_content('rodape', 'Por favor, crie uma página cujo nome seja "rodape" e grave o conteúdo do rodapé lá')?></td>
+    <td id="redes-sociais">
+      <a href="http://calcadosdiro.blogspot.com/" target="_blank"><?php Renders::render_image('footer/logo_blog.gif', array('width' => 16, 'height' => 16, 'border' => 0));?></a>
+      <a href="http://www.facebook.com/profile.php?id=100001737159103" target="_blank"><?php Renders::render_image('footer/logo_facebook.gif', array('width' => 16, 'height' => 16, 'style' => 'margin-left:5px;', 'border' => 0));?></a>
+      <a href="http://twitter.com/Dirocalcados" target="_blank"><?php Renders::render_image('footer/logo_twitter.gif', array('width' => 16, 'height' => 16, 'style' => 'margin-left:5px;', 'border' => 0));?></a>
+      <a href="http://www.orkut.com.br/Main#Profile?uid=13310873658463652765" target="_blank"><?php Renders::render_image('footer/logo_orkut.gif', array('width' => 16, 'height' => 16, 'style' => 'margin-left:5px;', 'border' => 0));?></a>
+    </td>
   </tr>
 </table>
 <?php
